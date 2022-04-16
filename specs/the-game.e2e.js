3d5d7a8fcc634b6e54c532067837e6ed977bede3
Alina Strashevichutse <astrashevichute@gmail.com>
2022-04-16 20:22:59 +0200

    add solution

--- specs/the-game.e2e.js
+++ specs/the-game.e2e.js
@@ -1,13 +1,25 @@
 describe('The game', function () {
     it('should get more than 100 points ', async function () {
-        await browser.url('http://localhost:63342/course-sut/arkanoid.html');
+        await browser.url('https://viktor-silakov.github.io/course-sut/arkanoid.html');
         await browser.pause(1000);
         await $('button=PLAY').click();
         await browser.waitUntil(async () => {
             // player logic
+            let ballLocation = await $('#ball').getLocation();
+            let padLocation =  await $('#pad').getLocation();
+            if(+padLocation.x <= +ballLocation.x){
+                await $('#pad').keys('D');       
+            } else if(+padLocation.x >= +ballLocation.x) { 
+                await $('#pad').keys('A');       
+            }
             const points = parseInt(await $('#points').getText(), 10);
             if (points > 100) return true
             console.log({ points })
         }, { timeout: 600000, interval: 10 })
     });
 })
+
+
+
+
+
